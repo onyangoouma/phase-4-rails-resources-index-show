980c2a2a67b1ae926256cc1d552442e582088b7f
onyangoouma <jacksonouma04@gmail.com>
2022-09-26 06:14:59 +0300

    completed

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+
+ActiveRecord::Schema.define(version: 2022_09_26_025054) do
+
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
